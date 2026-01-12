78c8e701be62f4900f099b74c4641a15a4bd5474
seungin <tmddls91@inha.edu>
2026-01-13 01:01:57 +0900

    [chore] Tailwind 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div>BReady Frontend</div>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-emerald-400">BReady</h1>
+    </div>
+  )
 }
