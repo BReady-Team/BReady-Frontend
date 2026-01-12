a01321c9c2493de8d7d5c24fe08c751a4ef8f14c
seungin <tmddls91@inha.edu>
2026-01-13 00:49:50 +0900

    [chore] ESLint, Prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist", "build", "node_modules"]),
+
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
+
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2021,
       globals: globals.browser,
     },
+
+    rules: {
+      "react/react-in-jsx-scope": "off",
+
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      "no-console": "off",
+    },
   },
-])
+]);
