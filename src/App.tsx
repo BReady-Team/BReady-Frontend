20c8c21f98000b25e07b31c26d439f6895d2bd33
seungin <tmddls91@inha.edu>
2026-01-17 22:28:44 +0900

    [feat] App.tsx에 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router'
+
 export default function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-emerald-400">BReady</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
