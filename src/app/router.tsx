87562390057a4809301d76bcb177aac72a7e6420
axoesnxm <sula9139@gmail.com>
2026-01-19 02:47:29 +0900

    [feat] plan 라우트 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,6 +4,7 @@ import LoginPage from '../features/auth/pages/LoginPage'
 import SignupPage from '../features/auth/pages/SignupPage'
 import MainLayout from '../components/layout/MainLayout'
 import AuthLayout from '../components/layout/AuthLayout'
+import PlanListPage from '../features/plan/pages/PlanListPage'
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,9 @@ export const router = createBrowserRouter([
   },
   {
     element: <MainLayout />,
-    children: [{ path: '/', element: <HomePage /> }],
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/plans', element: <PlanListPage /> },
+    ],
   },
 ])
