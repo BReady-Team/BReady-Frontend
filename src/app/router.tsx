1151f87a579d468a63feca94f97a4711c5c563a7
seungin <tmddls91@inha.edu>
2026-01-19 03:51:54 +0900

    [feat] 일정 상세 페이지 라우팅 연결

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
+
 import HomePage from '../features/home/pages/HomePage'
 import LoginPage from '../features/auth/pages/LoginPage'
 import SignupPage from '../features/auth/pages/SignupPage'
+import PlanDetailPage from '../features/plans/pages/PlanDetailPage'
+
 import MainLayout from '../components/layout/MainLayout'
 import AuthLayout from '../components/layout/AuthLayout'
 
@@ -15,6 +18,9 @@ export const router = createBrowserRouter([
   },
   {
     element: <MainLayout />,
-    children: [{ path: '/', element: <HomePage /> }],
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/plans/:planId', element: <PlanDetailPage /> },
+    ],
   },
 ])
