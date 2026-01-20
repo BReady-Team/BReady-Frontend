83c5362e220cf5a9b99a19d10b9fb7054f5d0edc
seungin <tmddls91@inha.edu>
2026-01-21 00:01:21 +0900

    [refactor] 영역에 따라 라우팅 리팩토링

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,34 +3,44 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '../features/home/pages/HomePage'
 import LoginPage from '../features/auth/pages/LoginPage'
 import SignupPage from '../features/auth/pages/SignupPage'
+
 import PlanDetailPageWrapper from '../features/plans/pages/PlanDetailPageWrapper'
-import MainLayout from '../components/layout/MainLayout'
-import AuthLayout from '../components/layout/AuthLayout'
-import StatsPage from '../features/stats/pages/StatsPage'
 import PlanListPage from '../features/plans/pages/PlanListPage'
 import PlanCreatePage from '@/features/plans/pages/PlanCreatePage'
+
+import StatsPage from '../features/stats/pages/StatsPage'
 import MyPage from '@/features/user/pages/MyPage'
 
+import MainLayout from '../components/layout/MainLayout'
+import AuthLayout from '../components/layout/AuthLayout'
+
+import ProtectedRoute from '../routes/ProtectedRoute'
+
 export const router = createBrowserRouter([
+  // 로그인 전 영역
   {
     element: <AuthLayout />,
     children: [
+      { path: '/', element: <HomePage /> },
       { path: '/login', element: <LoginPage /> },
       { path: '/signup', element: <SignupPage /> },
-      { path: '/', element: <HomePage /> },
     ],
   },
+
+  // 로그인 후 영역
   {
-    element: <MainLayout />,
+    element: <ProtectedRoute />,
     children: [
-      { path: '/plans', element: <PlanListPage /> },
-      { path: '/plans/new', element: <PlanCreatePage /> },
       {
-        path: '/plans/:planId',
-        element: <PlanDetailPageWrapper />,
+        element: <MainLayout />,
+        children: [
+          { path: '/plans', element: <PlanListPage /> },
+          { path: '/plans/new', element: <PlanCreatePage /> },
+          { path: '/plans/:planId', element: <PlanDetailPageWrapper /> },
+          { path: '/stats', element: <StatsPage /> },
+          { path: '/mypage', element: <MyPage /> },
+        ],
       },
-      { path: '/mypage', element: <MyPage /> },
-      { path: '/stats', element: <StatsPage /> },
     ],
   },
 ])
