ee71660ef70eb37f8dcd5407b30c0d2334998aa7
seungin <tmddls91@inha.edu>
2026-03-08 23:07:34 +0900

    [feat] 마이페이지 연동을 위한 API 함수 구현

--- src/features/user/api.ts
+++ src/features/user/api.ts
@@ -1,7 +1,35 @@
+import { http } from '@/lib/http'
 import type { MyPageDTO } from './types'
-import { mockMyPage } from './mock/mockMyPage'
+import { getStatsSummary } from '@/lib/api/stats'
+
+interface CommonResponse<T> {
+  message: string
+  data: T
+}
 
 export async function fetchMyPage(): Promise<MyPageDTO> {
-  // TODO
-  return new Promise(resolve => setTimeout(() => resolve(mockMyPage), 300))
+  const [userRes, stats] = await Promise.all([
+    http.get<CommonResponse<any>>('/api/v1/users/me'),
+    getStatsSummary('ALL'),
+  ])
+
+  const data = userRes.data.data
+
+  return {
+    profile: {
+      nickname: data.nickname,
+      email: data.email,
+      bio: data.bio ?? '',
+      avatarUrl: data.profileImageUrl ?? undefined,
+      joinedAt: data.joinedAt,
+    },
+
+    stats: {
+      totalPlans: stats.totalPlans,
+      totalSwitches: stats.totalSwitches,
+      totalSwitchLogs: stats.recentCount,
+    },
+
+    recentPlans: [],
+  }
 }
